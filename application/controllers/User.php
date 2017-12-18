3cd5522e309ec739cbfb464a74fa31a7311f725d
Digital_Mac Book_A <hsbchsbc@312-Digital-Mac-Book.local>
2017-12-19 03:33:02 +0800

    update user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 use \QCloud_WeApp_SDK\Auth\LoginService as LoginService;
+use \QCloud_WeApp_SDK\Constants as Constants;
 
 class User extends CI_Controller {
     public function index() {
